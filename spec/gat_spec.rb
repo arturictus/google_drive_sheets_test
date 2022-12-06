7c0a2c4cd6ec0e52f62b9a426ad0d1010518e80c
Artur Panach <1930175+arturictus@users.noreply.github.com>
2022-12-06 21:34:36 +0000

    Save

--- spec/gat_spec.rb
+++ spec/gat_spec.rb
@@ -2,7 +2,7 @@
 
 require "./spec/spec_helper"
 
-RSpec.describe Gat do
+RSpec.describe Gat do # rubocop:disable Metrics/BlockLength
   it "has a version number" do
     expect(Gat::VERSION).not_to be nil
   end
